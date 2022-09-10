8d86824f6a19a1a2b3e2799f7eaefbf4568f48b7
douglas-robert-hill <douglasroberthill@gmail.com>
2022-09-10 19:38:33 +0100

    run script

---
+++ code/run.py
@@ -0,0 +1,38 @@
+
+# Function Import 
+from data_loader import * 
+from genetic_algo_class import * 
+
+# Config
+tickers = ['AAPL', 'TSLA', 'MSFT']
+population = 150
+risk_free_rate = 2
+generations = 40
+crossover_rate = 0.4
+mutation_rate = 0.01
+elite_rate = 0.25
+
+# Load Data
+stock_data = fetch_data(
+    tickers = tickers, 
+    start_date = '2019-02-01',
+    end_date = '2020-12-01'
+    )
+
+
+# Initialise GA
+ga = genetic_algorithm(
+    data = stock_data,
+    risk_free_rate = risk_free_rate, 
+    population = population, 
+    generations = generations, 
+    crossover_rate = crossover_rate, 
+    mutation_rate = mutation_rate, 
+    elite_rate = elite_rate
+)
+
+# Run GA
+ga.optimise_weights()
+
+# View Optimal Weights
+ga.optimal_weight
